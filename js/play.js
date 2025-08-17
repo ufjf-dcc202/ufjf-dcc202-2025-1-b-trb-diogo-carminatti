9d3dc7162d43d7d0fb4548dc684e92743ad7bba2
dcarminatti <dctti.amarante@gmail.com>
2025-08-17 15:23:20 -0300

    criado logica para renderizacao de obstaculos no grid

--- js/play.js
+++ js/play.js
@@ -9,11 +9,39 @@ const gameBoard = document.getElementById("game-board");
 const toolsBoard = document.getElementById("tools-board");
 const seedsBoard = document.getElementById("seeds-board");
 
+let obstacles = [];
+
+function onStartGame() {
+  createGameBoard();
+}
+
 function createGameBoard() {
   for (let i = 0; i < 144; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     cell.dataset.index = i;
+
+    const obstacle = createObstacles();
+    if (obstacle) {
+      cell.appendChild(obstacle);
+    }
+
     gameBoard.appendChild(cell);
   }
 }
+
+function createObstacles() {
+  const obstacleTypes = ["rock", "bush", "none"];
+  const obstacleElement = document.createElement("div");
+  const randomType =
+    obstacleTypes[Math.floor(Math.random() * obstacleTypes.length)];
+
+  if (randomType !== "none") {
+    obstacleElement.classList.add("obstacle", randomType);
+    return obstacleElement;
+  } else {
+    return null;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", onStartGame);
