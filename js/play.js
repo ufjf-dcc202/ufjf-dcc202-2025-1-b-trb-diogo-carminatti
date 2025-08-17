3934faff08164000730b2e1685faa4e6d022b13f
dcarminatti <dctti.amarante@gmail.com>
2025-08-17 15:38:33 -0300

    criado logica de renderizacao das ferramentas e de selecao da ferramenta atual

--- js/play.js
+++ js/play.js
@@ -1,3 +1,5 @@
+import { getTools } from "./tools.js";
+
 const state = {
   money: 100,
   currentTool: null,
@@ -13,6 +15,7 @@ let obstacles = [];
 
 function onStartGame() {
   createGameBoard();
+  createGameTools();
 }
 
 function createGameBoard() {
@@ -38,10 +41,46 @@ function createObstacles() {
 
   if (randomType !== "none") {
     obstacleElement.classList.add("obstacle", randomType);
+    obstacleElement.addEventListener("click", handleObstacleClick);
     return obstacleElement;
   } else {
     return null;
   }
+}
+
+function handleObstacleClick(event) {
+  const obstacle = event.target;
+  if (obstacle.classList.contains("obstacle")) {
+    obstacle.remove();
+  }
+}
+
+function createGameTools() {
+  const tools = getTools();
+  tools.forEach((tool) => {
+    const toolElement = document.createElement("div");
+    toolElement.classList.add("tool", tool.class);
+    toolElement.dataset.id = tool.id;
+    toolElement.addEventListener("click", handleToolClick);
+    toolsBoard.appendChild(toolElement);
+  });
+}
+
+function handleToolClick(event) {
+  const tool = event.target;
+  const toolId = tool.dataset.id;
+
+  if (toolId === state.currentTool) {
+    state.currentTool = null;
+    tool.classList.remove("selected");
+    return;
+  }
+
+  state.currentTool = toolId;
+  toolsBoard.querySelectorAll(".tool.selected").forEach((selectedTool) => {
+    selectedTool.classList.remove("selected");
+  });
+  tool.classList.add("selected");
 }
 
 document.addEventListener("DOMContentLoaded", onStartGame);
--- js/tools.js
+++ js/tools.js
@@ -1,14 +1,21 @@
 const tools = [
   {
     id: 1,
-    name: "Can",
+    name: "Watering Can",
+    class: "watering-can",
   },
   {
     id: 2,
     name: "Hoe",
+    class: "hoe",
   },
   {
     id: 3,
     name: "Pickaxe",
+    class: "pickaxe",
   },
 ];
+
+export function getTools() {
+  return [...tools];
+}
